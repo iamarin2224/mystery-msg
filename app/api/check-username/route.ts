e93977e88f1cdf327dad254d980417e21603b59a
Arin Das <iamarindas@gmail.com>
2025-12-25 13:55:46 +0530

    Complete username check and verify route

---
+++ app/api/check-username/route.ts
@@ -0,0 +1,54 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/users.models";
+import { z } from "zod"
+import { usernameValidation } from "@/schemas/signUpSchema";
+
+//we devise a method which takes username as paramter which must satisfy the username validation
+const UsernameQuerySchema = z.object({
+    username: usernameValidation
+})
+
+export async function GET(request:Request) {
+    await dbConnect()
+
+    try {
+        //extract the username from the request url
+        const {searchParams} = new URL(request.url)
+        const queryParam = searchParams.get('username')
+
+        //check using zod
+        const result = UsernameQuerySchema.safeParse(queryParam)
+
+        if (!result.success) {
+            return Response.json({
+                success: false,
+                message: result.error.issues[0].message,
+                },{ status: 400 }
+            )
+        }
+
+        const {username} = result.data
+
+        const existingVerifiedUser = await UserModel.findOne({username, isVerified: true})
+
+        if (existingVerifiedUser){
+            return Response.json({
+                success: false,
+                message: "User with given username exists"
+            },{status: 500})
+        }
+
+        return Response.json({
+            success: true,
+            message: "Username is unique",
+            },{ status: 200 }
+        ) 
+
+    } catch (error) {
+        console.error("Error checking username: ", error)
+        return Response.json({
+            success: false,
+            message: "Error checking username"
+        },{status: 500})
+    }
+}
---
+++ app/api/verify/route.ts
@@ -0,0 +1,50 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/users.models";
+
+export async function GET(request:Request) {
+    await dbConnect()
+
+    try {
+        const {username, code} = await request.json()
+
+        const user = await UserModel.findOne({username})
+
+        if (!user){
+            return Response.json({
+                success: false,
+                message: "User with given username not found"
+            },{status: 500})
+        }
+
+        const isCodeCorrect = user.verifyCode === code
+        const isCodeExpired = new Date(user.verifyCodeExpiry) < new Date()
+
+        if (isCodeCorrect && !isCodeExpired){
+            user.isVerified = true
+            await user.save()
+            return Response.json({
+                success: true,
+                message: "User verified successfully"
+            },{status: 200})
+        }
+        else if (isCodeExpired){
+            return Response.json({
+                success: false,
+                message: "Verification code expired, please signup again"
+            },{status: 500})
+        }
+        else {
+            return Response.json({
+                success: false,
+                message: "Verification code is incorrect"
+            },{status: 500})
+        }
+
+    } catch (error) {
+        console.error("Error checking username: ", error)
+        return Response.json({
+            success: false,
+            message: "Error checking username"
+        },{status: 500})
+    }
+}
