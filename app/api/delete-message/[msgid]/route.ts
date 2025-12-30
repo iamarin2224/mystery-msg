fd16f978f4a51d177aa40d9f9773e58fddf5fb5b
Arin Das <iamarindas@gmail.com>
2025-12-30 18:38:41 +0530

    Complete delete route

---
+++ app/api/delete-message/[msgid]/route.ts
@@ -0,0 +1,47 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/option";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/users.models";
+import { User } from "next-auth";  
+
+export async function DELETE(req: Request, {params} : {params : {msgid: String}}) {
+    const msgId = params.msgid
+
+    await dbConnect() 
+
+    const session = await getServerSession(authOptions)
+    const user:User = session?.user 
+
+    if (!session || !user){
+        return Response.json({
+            success: false,
+            message: "User not authorized"
+        },{status: 401})
+    }
+
+    try {
+        const updatedResult = await UserModel.updateOne(
+            {_id: user._id},
+            {$pull: {messages: {_id: msgId}}} //pull method used to pull out one item of an array
+        )
+
+        if (updatedResult.modifiedCount === 0){
+            return Response.json({
+                success: false,
+                message: "Message not found"
+            },{status: 404})
+        }
+
+        return Response.json({
+            success: true,
+            messages: "Message deleted successfully"
+        },{status: 200})
+
+    } catch (error) {
+        console.error("Failed to delete message", error);
+        return Response.json({
+            success: false,
+            message: "Failed to delete message"
+        },{status: 500})
+    }
+}
