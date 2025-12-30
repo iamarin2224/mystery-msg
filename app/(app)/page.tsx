b4693d41e9f77d5b4a2c339b23fd1bebb502cacf
Arin Das <iamarindas@gmail.com>
2025-12-30 21:41:17 +0530

    Complete Home Page

---
+++ app/(app)/page.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Autoplay from 'embla-carousel-autoplay'
+import messages from "@/data/messages.json"
+
+export default function Home() {
+  return (
+    <>
+      <main className="grow flex flex-col items-center justify-center px-4 md:px-24 py-12 bg-gray-800 text-white" >
+
+        <section className="text-center mb-8 md:mb-12">
+          <h1 className="text-3xl md:text-5xl font-bold">
+            Dive into the World of Mysterious and Annonymous Messages
+          </h1>
+          <p className="mt-3 md:mt-4 text-base md:text-lg">
+            Mystery Message - Where your identity remains a secret.
+          </p>
+        </section>
+
+        <Carousel 
+          plugins={[Autoplay({delay: 2500})]}
+          className="w-full max-w-xs"
+        >
+          <CarouselContent>
+            {messages.map((message, index) => (
+              <CarouselItem key={index}>
+                <div className="p-1">
+                  <Card>
+                    <CardHeader>
+                      <CardTitle>{message.title}</CardTitle>
+                    </CardHeader>
+                    <CardContent className="flex items-center justify-center p-6">
+                      <span className="text-2xl font-semibold mx-4">{message.content}</span>
+                    </CardContent>
+                    <CardFooter>
+                      {message.received}
+                    </CardFooter>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          {/* <CarouselPrevious />
+          <CarouselNext /> */}
+        </Carousel>
+      </main>
+
+      <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
+        © 2023 Mystery Message. All rights reserved.
+      </footer>
+    </>
+  );
+}
