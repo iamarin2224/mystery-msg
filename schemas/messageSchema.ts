3dd4d858d220ca64ed4a5a2bc91cb28606257e77
Arin Das <iamarindas@gmail.com>
2025-12-31 13:11:03 +0530

    Update message schema

--- schemas/messageSchema.ts
+++ schemas/messageSchema.ts
@@ -3,5 +3,5 @@ import {z} from 'zod'
 export const messageSchema = z.object({
     content: z.string()
             .min(6, {error: "Message must have atleat 6 characters"})
-            .max(1000, {error: "Message must not exceed 1000 characters"})
+            .max(500, {error: "Message must not exceed 500 characters"})
 })
