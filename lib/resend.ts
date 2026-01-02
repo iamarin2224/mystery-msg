53f2061c575d8ebde05be21fc8b5d4a8f8343402
Arin Das <iamarindas@gmail.com>
2026-01-02 14:08:32 +0530

    Modify resend

--- lib/resend.ts
+++ lib/resend.ts
@@ -1,15 +1,26 @@
 import { Resend } from 'resend';
 import { ApiResponse } from '@/types/ApiResponse';
 import VerificationEmail from '@/emails/VerificationEmail';
+import ForgotPassVerification from '@/emails/ForgotPassVerification';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendVerificationEmail(
     email: string,
     username: string,
-    otp: string
+    otp: string,
+    forgotPass: boolean = false
 ): Promise<ApiResponse>{
     try {
+        if (forgotPass){
+            await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email,
+            subject: 'Mystery Message Verfication Code',
+            react: ForgotPassVerification({username, otp}),
+            });
+            return {success: true, message: "Verification email send succesfully."}
+        }
         await resend.emails.send({
         from: 'onboarding@resend.dev',
         to: email,
